bbd7ad4aff82617ac1f3019885b45ba3b3d5269f
MaximeCHENET <maxime.chenet14@gmail.com>
2022-09-27 15:43:36 +0200

    portfolio

---
+++ function.js
@@ -0,0 +1,12 @@
+function changeDiv(evt, divName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    document.getElementById(divName).style.display = "flex";
+}
