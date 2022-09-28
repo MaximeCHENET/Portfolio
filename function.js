dac2aa5334feafef9894e43f4217a5faa55594af
MaximeCHENET <maxime.chenet14@gmail.com>
2022-09-28 16:42:56 +0200

    CV + Apprentissage + fix

--- function.js
+++ function.js
@@ -1,12 +1,12 @@
 function changeDiv(evt, divName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-    document.getElementById(divName).style.display = "flex";
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  document.getElementById(divName).style.display = "flex";
 }
