ab1206723d1f7e188264a7d7dca9d672c20e0326
MaximeCHENET <maxime.chenet14@gmail.com>
2022-12-08 11:37:16 +0100

    Refonte Design

--- function.js
+++ function.js
@@ -1,12 +1,24 @@
-function changeDiv(evt, divName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+//Display selected element
+const changeDiv = (evt, divName) => {
+  var tabcontent;
 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for(let divBloc of tabcontent) {
     divBloc.style.display = "none";
   }
 
   document.getElementById(divName).style.display = "flex";
+}
+
+
+//Display selected file
+const selectFile = (evt, fileName) => {
+  var fileContent;
+
+  fileContent = document.getElementsByClassName("fileContent");
+  for(let fileBloc of fileContent) {
+    fileBloc.style.display = "none";
+  }
+
+  document.getElementById(fileName).style.display = "flex";
 }
